de05c0bab5fb1eb190d09944eaf0ee33a89f6755
thanhnguyen <thanhnguyen95abc@gmail.com>
2025-01-17 10:26:23 +0700

    change testimonial on job page

--- src/components/jobs/JobBanner.tsx
+++ src/components/jobs/JobBanner.tsx
@@ -21,6 +21,23 @@ const CLIENT_INFO = [
   },
 ];
 
+export const TestimonialsData = [
+  {
+    comment: `“Shaun is a standout recruiter. From our first chat, he took a human-first approach, quickly identifying the perfect role and guiding me through every step. His dedication and support are unmatched. I highly recommend him!”`,
+    name: "Katy DeWolfe Teresczenko",
+    jobTitle: "Sales Director",
+  },
+  {
+    comment: `“Shaun is an exceptional recruiter. Over a year after our first conversation, he remembered my career goals and reached out with the perfect role. His communication and focus on my needs were outstanding. Highly recommend him!”`,
+    name: "Tony Mancuso",
+    jobTitle: "Sales Director",
+  },
+  {
+    comment: `Shaun took the time to understand my career goals, skills, and preferences, ensuring a seamless match with potential opportunities. His ability to create relationships on both sides of the recruitment process was above and beyond. He is trusting, collaborative and valuable, and I am grateful for this next career chapter through his partnership!`,
+    name: "Theresa Snyder",
+    jobTitle: "Strategic Accounts Director",
+  },
+];
 const ClientInfo = (info: { title: string; subTitle: string }) => {
   return (
     <Flex className="flex-col text-success gap-2 flex-1 text-center">
@@ -46,34 +63,21 @@ const JobBanner = () => {
           ))}
         </Flex>
       </Flex>
-      <Flex className="flex-col gap-4 md:flex-row md:gap-8">
-        <Card className="border-white text-white gap-1 lg:h-[380px]">
-          <Text className=" flex-1">
-            {`“Shaun is a standout recruiter. From our first chat, he took a human-first approach, quickly identifying the perfect role and guiding me through every step. His dedication and support are unmatched. I highly recommend him!”`}
-          </Text>
+      <Flex className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
+        {TestimonialsData.map((item) => {
+          return (
+            <Card
+              key={item.name}
+              className="border-white text-white gap-1 lg:h-[380px]"
+            >
+              <Text className=" flex-1">{item.comment}</Text>
 
-          <Text className="heading-4">{"Katy DeWolfe Teresczenko"}</Text>
-          <Text className="text-md-medium">{"Sales Director"}</Text>
-          <IcLinkedin className=" text-white" />
-        </Card>
-        <Card className="border-white text-white gap-1 lg:h-[380px]">
-          <Text className=" flex-1">
-            {`“Shaun is a standout recruiter. From our first chat, he took a human-first approach, quickly identifying the perfect role and guiding me through every step. His dedication and support are unmatched. I highly recommend him!”`}
-          </Text>
-
-          <Text className="heading-4">{"Katy DeWolfe Teresczenko"}</Text>
-          <Text className="text-md-medium">{"Sales Director"}</Text>
-          <IcLinkedin className=" text-white" />
-        </Card>
-        <Card className="border-white text-white gap-1 lg:h-[380px]">
-          <Text className=" flex-1">
-            {`“Shaun is a standout recruiter. From our first chat, he took a human-first approach, quickly identifying the perfect role and guiding me through every step. His dedication and support are unmatched. I highly recommend him!”`}
-          </Text>
-
-          <Text className="heading-4">{"Katy DeWolfe Teresczenko"}</Text>
-          <Text className="text-md-medium">{"Sales Director"}</Text>
-          <IcLinkedin className=" text-white" />
-        </Card>
+              <Text className="heading-4">{item.name.toUpperCase()}</Text>
+              <Text className="text-md-medium">{item.jobTitle}</Text>
+              <IcLinkedin className=" text-white" />
+            </Card>
+          );
+        })}
       </Flex>
     </Hero>
   );
