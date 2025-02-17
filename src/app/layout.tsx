929ae10cd002a4dc0f2e006fa63dec069aeb29d5
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2025-02-17 13:59:17 +0700

    update tracking gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,7 @@ export default function RootLayout({
       <head>
         <Script
           id="gtm-script"
-          strategy="afterInteractive"
-          async
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
