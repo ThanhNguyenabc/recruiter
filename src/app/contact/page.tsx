7db124e6fd140e2b042ef5861ec8c1b168a21465
ThanhNguyenabc <thanhnguyen95abc@gmail.com>
2025-12-30 14:45:56 +0700

    adjust contact title

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -41,7 +41,7 @@ const ContactPage = () => {
     <Hero className="flex-col w-full items-start gap-10 md:gap-20">
       <Flex className="flex-col gap-6 lg:max-w-[600px]">
         <Text type="h1" className="heading-1 whitespace-pre-line">
-          {`Contact us to get started`}
+          {`Contact us\nto get started`}
         </Text>
         <Text className="text-lg-medium text-secondary">
           {
